88d77a49d477ec2b53712e22d7842302e6953863
saltedsky <a.huang.011@gmail.com>
2015-03-15 09:40:02 -0700

    Update Deity.py

--- Deity.py
+++ Deity.py
@@ -3,6 +3,10 @@ import json
 class Deity:
 	def to_JSON(self):
 		return json.dumps(self,default=lambda o: o.__dict__,sort_keys=True,indent=4)
+	def write_JSON(self):
+		obj = self.to_JSON() + ",\n"
+		with open('data.json', 'a') as f:
+			f.write(obj)
 	def __init__(self,name,link):
 		self.name = name
 		self.link = link
