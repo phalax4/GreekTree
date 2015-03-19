3d8afd04d988aa0b830819997f639bc3f2b5e726
saltedsky <a.huang.011@gmail.com>
2015-03-18 23:40:19 -0700

    Update Scrape.py

--- Scrape.py
+++ Scrape.py
@@ -16,6 +16,7 @@ class Scrape:
 		content = url.read()
 		soup = BeautifulSoup(content)  #soup contains the Object to access page
 		self.tables = self.soup.findAll("table", {"class" : "wikitable"}) #Grabs the 3 wiki tables: Titans, 12 Gods and Primordial
+		self.lists = self.soup.findAll("div", {"class": "div-col columns column-count column-count-"})
 		#print type(soup)
 		print "Connection Success"
 	def extractWikiTables(self,x): #specify which wiki table to get
@@ -53,6 +54,29 @@ class Scrape:
 			#if(len(list2)>0):     ######Grabs the paragraph blurb
 			#	print list2[2]
 		return objlist
+	def extractWikiLists(self, x): #getting names of giants and personified concepts
+		objlist = []
+		searching = "ul > li"
+		soupb = BeautifulSoup(unicode(self.lists[x]))
+		for i in (soupb.select(searching)):
+			thing = i.find("a")
+			if thing != None:
+				link = self.mainUrl + thing.get('href')
+				name = thing.text.encode('utf-8')
+			else: #if there is no link
+				link = ""
+				name = i.text.encode('utf-8').split(" ")[0]
+			gen = ""
+			ty = ""
+			if x == 0:
+				ty = "Giant"
+			elif x == 1:
+				ty = "Personified Concept"
+			god = Deity(name,link)
+			god.generation = gen
+			god.typie = ty
+			objlist.append(god)
+		return objlist
 
 if __name__=='__main__':#testing purposes
 	scraper = Scrape()
@@ -62,3 +86,7 @@ if __name__=='__main__':#testing purposes
 		testList1 = scraper.extractWikiTables(i)
 		for i in testList1:
 			print i.getName() + " " + i.generation
+		for i in [0,1]:
+			testList2 = scraper.extractWikiLists(i)
+			for i in testList2:
+				print i.getName() + " " + i.typie
