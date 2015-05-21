d1020171bca19751016d3971a54db77917e68c84
saltedsky <a.huang.011@gmail.com>
2015-05-20 22:47:38 -0700

    added links for parents

--- Link.py
+++ Link.py
@@ -1,3 +1,4 @@
+from Scrape import *
 from Deity import *
 
 class Link:
@@ -5,31 +6,36 @@ class Link:
 		self.source = source
 		self.target = target
 		self.value = value
-	parent = ""
-	child = ""
 
 class MakeLinks:
 	def __init__(self, objlist):
 		self.objlist = objlist
 		self.linklist = []
-	
+
 	def find(self, name):
 		for i in range(len(self.objlist)):
 			if self.objlist[i].name == name:
 				return self.objlist[i]
 
-	def makeLinks(self): #links for children
-		#setting generation numbers
-		first = self.find("Gaia")
-		first.group = 0
-		self.generations(first)
-		
+	def makeLinks(self): 
+		#getting generation numbers
+		#nyx = self.find("Nyx")
+		#nyx.group = 0
+		#self.generations(nyx)
 		for i in range(len(self.objlist)):
 			deity = self.objlist[i]
 			#links for children
 			for child in deity.children:
 				for j in range(len(self.objlist)):
-					if str(self.objlist[j].name) == str(child):
+					if self.objlist[j].name == child:
+						target = j
+						link = Link(i, target, 1)
+						self.linklist += [link]
+						break
+			#links for parents
+			for parent in deity.parents:
+				for j in range(len(self.objlist)):
+					if self.objlist[j].name == parent:
 						target = j
 						link = Link(i, target, 1)
 						self.linklist += [link]
@@ -44,9 +50,11 @@ class MakeLinks:
 							link = Link(i, target, 2)
 							self.linklist += [link]
 							break
+
+
 	def generations(self,deity):
 		if deity.children != []:
-			if ("Titans" in deity.children):
+			if ("Titans" in deity.children) or ("The Titans" in deity.children):
 				for god in self.objlist:
 					if god.attribute == "Titan":
 						deity.children += [god.name]
@@ -54,13 +62,13 @@ class MakeLinks:
 				for god in self.objlist:
 					if god.attribute == "Gigante":
 						deity.children += [god.name]
+
 			g = deity.group + 1
 			for child in deity.children:
 				for j in range(len(self.objlist)):
-					if str(self.objlist[j].name) == str(child):
+					if self.objlist[j].name == child:
 						newdeity = self.objlist[j]
 						newdeity.group = g
 						self.generations(newdeity)				
 						break
-
 
